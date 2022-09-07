f33ae0a5a168418f90f70fc3d493e375f554454f
Emre <aka548548@gmail.com>
2022-09-07 21:00:20 +0300

    form is created

--- src/app/components/create-special-client-request/create-special-client-request.component.ts
+++ src/app/components/create-special-client-request/create-special-client-request.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ClientRequestService } from 'src/app/services/client-request.service';
 import {NgxSpinnerService} from 'ngx-spinner';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-create-special-client-request',
@@ -11,16 +13,26 @@ import {NgxSpinnerService} from 'ngx-spinner';
 export class CreateSpecialClientRequestComponent implements OnInit {
 
   clientRequestForm: FormGroup;
+  categories: Category[];
+
   constructor(private fb: FormBuilder,
     private clientRequestService: ClientRequestService,
-    private spinner: NgxSpinnerService) { }
+    private spinner: NgxSpinnerService,
+    private categoryService: CategoryService) { }
 
   ngOnInit(): void {
     this.createForm();
+    this.getSoftwareCategories();
+    this.clientRequestForm.valueChanges.subscribe({
+      next: (value) => {
+        console.log(value);
+      }
+    })
   }
 
   createForm() {
     this.clientRequestForm = this.fb.group({
+      categoryId: [0, [Validators.required]],
       title: ["", [Validators.required]],
       description: ["", [Validators.required]],
       deadline: ["", [Validators.required]],
@@ -35,6 +47,20 @@ export class CreateSpecialClientRequestComponent implements OnInit {
         this.spinner.hide();
       },
       error: (err) => {
+        this.spinner.hide();
+      }
+    });
+  }
+
+  getSoftwareCategories() {
+    this.spinner.show();
+    this.categoryService.getCategories().subscribe({
+      next: (value) => {
+        this.categories = value;
+        this.spinner.hide();
+      },
+      error: (err) => {
+        console.log(err);
         this.spinner.hide();
       }
     });
