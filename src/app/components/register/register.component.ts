8472561dbc6e0f6d80d4e9de53b7aee77814dc1a
Emre <aka548548@gmail.com>
2022-09-15 00:56:05 +0300

    register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  credentials: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+    this.credentials.valueChanges.subscribe({ next: (value) => console.log(value) });
+  }
+
+  createRegisterForm() {
+    this.credentials = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      userName: ["", [Validators.required]],
+      password: ["", [Validators.required, Validators.minLength(9)]],
+      firstName: ["", [Validators.required]],
+      secondName: [""],
+      lastName: ["", [Validators.required]]
+    });
+  }
+
+  register() {
+
   }
 
 }
