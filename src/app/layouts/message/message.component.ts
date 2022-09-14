8c54374271e4f474875d23a2ac00b1d3d719b667
Emre <aka548548@gmail.com>
2022-09-14 15:54:23 +0300

    message

--- src/app/layouts/message/message.component.ts
+++ src/app/layouts/message/message.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from 'src/app/services/auth.service';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-message',
@@ -9,9 +12,29 @@ export class MessageComponent implements OnInit {
 
   isOpen: boolean = true;
 
-  constructor() { }
+  messages: any[];
+
+  constructor(private messageService: MessageService,
+    private spinner: NgxSpinnerService,
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+    if (this.authService.isAuthenticated())
+      this.getMyMessages();
+  }
+
+  getMyMessages() {
+    this.spinner.show();
+    this.messageService.getMyMessages().subscribe({
+      next: (value) => {
+        this.messages = value;
+        this.spinner.hide();
+      },
+      error: (err) => {
+        console.log(err);
+        this.spinner.hide();
+      }
+    });
   }
 
 }
---
+++ src/app/services/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getMyMessages(): Observable<any[]> {
+    return this.httpClient.get<any[]>(environment.apiRoute + "messages");
+  }
+}
