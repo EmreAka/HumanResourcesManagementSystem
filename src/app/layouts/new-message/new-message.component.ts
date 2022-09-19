070e3464586837ca0e38ce0c2a5594fbc4e655b8
Emre <aka548548@gmail.com>
2022-09-19 19:01:15 +0300

    usernames are fetched

--- src/app/layouts/new-message/new-message.component.ts
+++ src/app/layouts/new-message/new-message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'src/app/services/message.service';
 
 @Component({
   selector: 'app-new-message',
@@ -9,9 +10,20 @@ export class NewMessageComponent implements OnInit {
 
   isMessagesOpen: boolean = false;
 
-  constructor() { }
+  userNames: any[];
+
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getUserNames();
+  }
+
+  getUserNames() {
+    this.messageService.getUserNames().subscribe({
+      next: (value) => {
+        this.userNames = value;
+      }
+    });
   }
 
 }
